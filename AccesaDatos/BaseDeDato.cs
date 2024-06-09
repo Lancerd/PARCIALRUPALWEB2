09149569e971811e985c6980a91c99a8a3716624
LUEDALVE <luisalbor@coruniamericana.edu.co>
2024-06-09 17:38:22 -0500

    Cambio de configuración de las capas

--- AccesaDatos/BaseDeDato.cs
+++ AccesaDatos/BaseDeDato.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace AccesaDatos
 {
     public class BaseDeDato
     {
+        string cadConex;
+        OleDbConnection cn;
+        OleDbCommand cmd;
+
+        public int BdCodeError {get; set;}
+        public int BdMsgError {get; set;}
     }
 }
